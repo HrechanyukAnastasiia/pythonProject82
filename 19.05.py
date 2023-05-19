113176abd804d9c5490682827da8e6096f93230c
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-19 23:58:32 +0300

    shop

--- 19.05.py
+++ 19.05.py
@@ -1,81 +1,77 @@
 class Clothing:
-    def __init__(self, material, name, price, size):
+    def __init__(self , material , name , price , size):
         self.material = material
         self.name = name
         self.price = price
         self.size = size
 
 class Store:
-    def __init__(self, name, address):
+    def __init__(self , name , address):
         self.name = name
         self.address = address
         self.inventory = []
-    def add_item(self, item):
+    def add_item(self , item):
         self.inventory.append(item)
-    def remove_item(self, item):
+    def remove_item(self , item):
         self.inventory.remove(item)
-    def get_items(self):
+    def get_items(self ):
         return self.inventory
-    def search_items(self, key):
-        return [item for item in self.inventory if key in item.name]
-
+    def search_items(self , key):
+        dictionary = []
+        for dict in self.inventory:
+            if key in dict.name:
+                dictionary.append(key)
+        for value in dictionary:
+            if len(dictionary) == 0:
+                print(f"Немає {key} в наявності")
+            else:
+                print(f"Ось що ми знайшли по Вашому запиту: {value}")
 
 class Customer:
-    def __init__(self, name, budget):
+    def __init__(self , name, budget):
         self.name = name
         self.budget = budget
         self.cart = []
-    def add_to_cart(self,item):
+
+    def add_to_cart(self , item):
         if item.price <= self.budget:
-            self.cart.append(item)
-            self.budget -= item.price
-            print(f'{item.name} додано до кошика')
+         self.cart.append(item)
+         print(f"{item.name} {item.size} -> ({item.price} грн) була добавлена в корзину")
         else:
-            print('Недостатньо коштів')
-    def remove_from_cart(self, item):
-        if item in self.cart:
-            self.cart.remove(item)
-            self.budget += item.price
-            print(f'{item.name} видалено з кошика')
-        else:
-            print(f'{item.name} немає в кошику')
+            print("У вас не достатньо коштів")
     def view_cart(self):
-        if len(self.cart) == 0:
-            print('Кошик порожній')
-        else:
-            print('Кошик:')
-            for item in self.cart:
-                print(f'{item.name} ({item.size}): {item.price}')
+      if len(self.cart) == 0:
+          print("У вас пусто в корзині")
+      else:
+        print("\nКорзина:")
+        for dictionary in self.cart:
+         print(f"{dictionary.name} {dictionary.size} -> {dictionary.price}")
+
+    def remove_from_cart(self , item):
+        self.cart.remove(item)
+        print(f"\n{item.name} {item.size} -> була видалена з корзини")
     def checkout(self):
-        total_price = sum(item.price for item in self.cart)
-        if total_price <= self.budget:
-            self.cart = []
-            self.budget -= total_price
-            print('Оплата пройшла успішно!')
+        count = 0
+        if len(self.cart) == 0:
+            print("У вас пусто в карзині")
         else:
-            print('Недостатньо коштів для оформлення замовлення.')
+            print("\nПокупки:")
+            for dictionary in self.cart:
+                 print(f"{dictionary.name} {dictionary.size} -> {dictionary.price}")
+                 count += dictionary.price
+            print(f"Сума: {count}")
+
 
 
 shop = Store("LC WAIKIKI", "вулиця 600-річчя")
-print(shop.name + ', ' + shop.address)
-shop.add_item(Clothing("Бавовна", "Футболка", 10, "M"))
-shop.add_item(Clothing("Вовна", "Шорти", 40, "XL"))
-shop.add_item(Clothing("Поліестр", "Майка", 100, "L"))
-print()
-
-customer = Customer("Настя", 10)
-print(f"Покупець: {customer.name}, Баланс: {customer.budget}")
-print()
-results = shop.search_items("Шорти")
-if results:
-    customer.add_to_cart(results[0])
-results1 = shop.search_items("Майка")
-if results:
-    customer.add_to_cart(results1[0])
-
+item_1 = Clothing("Кожа" , "Сумка" , 500)
+item_2 = Clothing("Хлопок" , "Кофта"  , 400)
+shop.add_item(item_1)
+shop.add_item(item_2)
+customer = Customer("Настя" , 1000)
+customer.add_to_cart(item_1)
+customer.add_to_cart(item_2)
 customer.view_cart()
-customer.remove_from_cart(results[0])
-customer.remove_from_cart(results1[0])
-customer.view_cart()
-print()
 customer.checkout()
+shop.search_items("Куртка")
+shop.search_items("Кофта")
