3b69d7c291251b7afc90ccd22e06c0761dba3d99
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-20 00:24:05 +0300

    shop

--- 19.05.py
+++ 19.05.py
@@ -36,7 +36,7 @@ class Customer:
     def add_to_cart(self , item):
         if item.price <= self.budget:
          self.cart.append(item)
-         print(f"{item.name} {item.size} -> ({item.price} грн) була добавлена в корзину")
+         print(f"{item.name} {item.size} ({item.price} грн) була добавлена в корзину")
         else:
             print("У вас не достатньо коштів")
     def view_cart(self):
@@ -45,28 +45,26 @@ class Customer:
       else:
         print("\nКорзина:")
         for dictionary in self.cart:
-         print(f"{dictionary.name} {dictionary.size} -> {dictionary.price}")
+         print(f"{dictionary.name} {dictionary.size} - {dictionary.price}")
 
     def remove_from_cart(self , item):
         self.cart.remove(item)
-        print(f"\n{item.name} {item.size} -> була видалена з корзини")
+        print(f"\n{item.name} {item.size} - була видалена з корзини")
     def checkout(self):
         count = 0
         if len(self.cart) == 0:
-            print("У вас пусто в карзині")
+            print("У вас пусто в кoрзині")
         else:
             print("\nПокупки:")
             for dictionary in self.cart:
-                 print(f"{dictionary.name} {dictionary.size} -> {dictionary.price}")
+                 print(f"{dictionary.name} {dictionary.size} - {dictionary.price}")
                  count += dictionary.price
             print(f"Сума: {count}")
 
-
-
 shop = Store("LC WAIKIKI", "вулиця 600-річчя")
 print(shop.name + ', ' + shop.address)
-item_1 = Clothing("Кожа" , "Куртка" , 500 , "XL")
-item_2 = Clothing("Хлопок" , "Кофта"  , 400 , "L")
+item_1 = Clothing("Поліестер" , "Штани" , 500 , "M")
+item_2 = Clothing("Бавовна" , "Кофта"  , 400 , "L")
 shop.add_item(item_1)
 shop.add_item(item_2)
 customer = Customer("Настя" , 1000)
@@ -74,5 +72,5 @@ customer.add_to_cart(item_1)
 customer.add_to_cart(item_2)
 customer.view_cart()
 customer.checkout()
-shop.search_items("Куртка")
+shop.search_items("Штани")
 shop.search_items("Кофта")
